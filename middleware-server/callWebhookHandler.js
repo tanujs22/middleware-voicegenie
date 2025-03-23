7af14cb187d759e41dc736312295345d76b37c75
Tanuj Srivastava <tanujsrivastav22@gmail.com>
2025-03-23 15:39:49 +0530

    err

--- middleware-server/callWebhookHandler.js
+++ middleware-server/callWebhookHandler.js
@@ -9,6 +9,7 @@ async function sendStatusCallback(statusCallbackUrl, callSid, status) {
     });
     console.log(`Call status '${status}' sent successfully.`);
   } catch (err) {
+    console.log(err)
     console.error('Error sending call status:', err.message);
   }
 }
@@ -19,6 +20,7 @@ async function sendHangupCallback(hangupUrl, payload) {
     await axios.post(hangupUrl, payload);
     console.log('Hangup event sent successfully.');
   } catch (err) {
+    console.log(err)
     console.error('Error sending hangup event:', err.message);
   }
 }
